48ebd9fa75aa7f8329193b9dce0d8f012e9efacc
Gabriel Horner <gabriel.horner@gmail.com>
2025-05-10 00:37:57 -0400

    fix: invalid property types for llm response

--- src/cldwalker/structured_chat/property.cljs
+++ src/cldwalker/structured_chat/property.cljs
@@ -17,17 +17,17 @@
 
   ;; LLMResponse properties
    ::prompt
-   {:logseq.property/type :string
+   {:logseq.property/type :default
     :db/cardinality :db.cardinality/one
     :build/properties
     {:logseq.property/description "Full prompt used to generate response"}}
    ::llmProvider
-   {:logseq.property/type :string
+   {:logseq.property/type :default
     :db/cardinality :db.cardinality/one
     :build/properties
     {:logseq.property/description "LLM provider for a response"}}
    ::model
-   {:logseq.property/type :string
+   {:logseq.property/type :default
     :db/cardinality :db.cardinality/one
     :build/properties
     {:logseq.property/description "LLM Model of a provider"}}
@@ -37,7 +37,7 @@
     :build/properties
     {:logseq.property/description "Total time to generate and receive response (ms)"}}
    ::tokens
-   {:logseq.property/type :integer
+   {:logseq.property/type :number
     :db/cardinality :db.cardinality/one
     :build/properties
     {:logseq.property/description "Response's tokens"}}})
